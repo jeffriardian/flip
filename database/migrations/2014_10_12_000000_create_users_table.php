e1b1235685a8d6823740a6df14711e2222f87f6a
jeffriardian <60500344+jeffriardian@users.noreply.github.com>
2023-08-21 04:44:41 +0700

    remove remember token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->integer('balance')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
