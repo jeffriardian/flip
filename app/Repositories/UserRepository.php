c0937ccae03334b7c2025d8469b782f9894f19cc
jeffriardian <60500344+jeffriardian@users.noreply.github.com>
2023-08-27 23:38:17 +0700

    remove unnecessary function

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -49,34 +49,6 @@ class UserRepository implements UserInterface
         }
     }
 
-    /*public function login(LoginRequest $request)
-    {
-        $credentials = $request->only('username');
-        if ($token = $this->guard()->attempt($credentials)) {
-            return response()->json(['status' => 'success', 'token' => $token, 'message' => 'Logged in Successfully.'], 200)->header('Authorization', $token);
-        }
-        return response()->json(['error' => 'login_error'], 401);
-    }
-
-    public function logout()
-    {
-        $this->guard()->logout();
-        return response()->json([
-            'status' => 'success',
-            'msg' => 'Logged out Successfully.'
-        ], 200);
-    }
-
-    public function refresh()
-    {
-        if ($token = $this->guard()->refresh()) {
-            return response()
-                ->json(['status' => 'successs'], 200)
-                ->header('Authorization', $token);
-        }
-        return response()->json(['error' => 'refresh_token_error'], 401);
-    }*/
-
     public function guard()
     {
         return User::guard();
